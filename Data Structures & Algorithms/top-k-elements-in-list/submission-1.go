8073ec58665824bcba622a3bef7988decd3c3736
Felipe Flores <felipeguilhermefs@gmail.com>
2026-09-24 00:22:32 +0200

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.go
@@ -0,0 +1,22 @@
+func topKFrequent(nums []int, k int) []int {
+	counter := make(map[int]int)
+	for _, num := range nums {
+		counter[num]++
+	}
+
+	freq := make([][]int, len(nums)+1)
+	for num, count := range counter {
+		freq[count] = append(freq[count], num)
+	}
+
+	res := []int{}
+	for i := len(freq) - 1; i > 0; i-- {
+		for _, num := range freq[i] {
+			res = append(res, num)
+			if len(res) == k {
+				return res
+			}
+		}
+	}
+	return res
+}
